ddfc41f7cd247cccaaf83f79ec6834e5f735f7ba
gamepkw <game9074@gmail.com>
2023-09-05 20:11:15 +0700

    Initial commit

--- .github/workflow/deploy.ps1
+++ .github/workflow/deploy.ps1
@@ -1,5 +1,5 @@
 $repositoryUrl = "https://github.com/gamepkw/atm4.git"
-$destinationDirectory = "C:/Users/admin/Project_GOs/atm4_deploy2"
+$destinationDirectory = "C:/Users/admin/Project_GOs/atm4_deploy3"
 git clone $repositoryUrl $destinationDirectory
 cd $destinationDirectory
 npm install
