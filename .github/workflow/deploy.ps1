8b2016ff973fa34feb90d1de6d721f35e09e582b
gamepkw <game9074@gmail.com>
2023-09-05 20:29:24 +0700

    Initial commit

--- .github/workflow/deploy.ps1
+++ .github/workflow/deploy.ps1
@@ -1,8 +1,51 @@
-$repositoryUrl = "https://github.com/gamepkw/atm4.git"
-$destinationDirectory = "C:/Users/admin/Project_GOs/atm4_deploy3"
-git clone $repositoryUrl $destinationDirectory
-cd $destinationDirectory
-npm install
-npm run build
-npm test
-npm start
+# $repositoryUrl = "https://github.com/gamepkw/atm4.git"
+# $destinationDirectory = "C:/Users/admin/Project_GOs/atm4_deploy3"
+# git clone $repositoryUrl $destinationDirectory
+# cd $destinationDirectory
+# npm install
+# npm run build
+# npm test
+# npm start
+
+pipeline {
+    agent any // You can specify the agent to run on
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm // Checkout the source code
+            }
+        }
+
+        stage('Set up Node.js') {
+            steps {
+                // Configure Node.js setup (if required)
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                sh 'npm install' // Run npm install
+            }
+        }
+
+        stage('Build Project') {
+            steps {
+                sh 'npm run build' // Run npm run build
+            }
+        }
+
+        stage('Run Tests') {
+            steps {
+                sh 'npm test' // Run npm test
+            }
+        }
+
+        stage('Deploy to Production') {
+            steps {
+                sh './deploy.ps1' // Run the PowerShell script
+            }
+        }
+    }
+}
+
